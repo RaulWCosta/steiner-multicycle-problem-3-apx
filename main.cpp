f27019424f840220f6b253135489ecb2f5e763f4
Raul Wagner Costa <raul.wagner@live.com>
2023-04-25 21:43:41 -0300

    Add exact solver init

--- main.cpp
+++ main.cpp
@@ -7,52 +7,97 @@
 #include "src/relaxed_solver_surviv_net.h"
 
 #include <vector>
-
-// #include <lemon/list_graph.h>
-
+#include <utility>
+#include <tuple>
 
 #include <lemon/lmath.h>
+#include <lemon/core.h>
+#include <lemon/graph_to_eps.h>
+#include <lemon/full_graph.h>
+#include <lemon/concepts/graph_components.h>
 
-#include<lemon/graph_to_eps.h>
-#include<lemon/list_graph.h>
+// int test()
+// {
+//     std::vector<int> source2sink = { 2, 3 };
+//     std::vector<Vertex> vertices = {
+//         Vertex(0.0f, 0.0f),
+//         Vertex(1.0f, 0.0f),
+//         Vertex(1.0f, 1.0f),
+//         Vertex(0.0f, 1.0f),
+//     };
 
-int test()
-{
-    std::vector<int> source2sink = { 2, 3 };
-    std::vector<Vertex> vertices = {
-        Vertex(0.0f, 0.0f),
-        Vertex(1.0f, 0.0f),
-        Vertex(1.0f, 1.0f),
-        Vertex(0.0f, 1.0f),
-    };
+//     GRBEnv env = new GRBEnv(true);
+//     env.set("LogFile", "mip1.log");
+//     env.start();
 
-    GRBEnv env = new GRBEnv(true);
-    env.set("LogFile", "mip1.log");
-    env.start();
+//     // Create an empty model
+//     GRBModel model = GRBModel(env);
 
-    // Create an empty model
-    GRBModel model = GRBModel(env);
+//     RelaxedSurvivableNetworkSolver solver(model, 4, source2sink, vertices);
 
-    RelaxedSurvivableNetworkSolver solver(model, 4, source2sink, vertices);
+//     solver.solve(model);
 
-    solver.solve(model);
+//     return 0;
+// }
 
-    return 0;
+float vertices_distance(const std::pair<float, float>& a, const std::pair<float, float>& b) {
+    float dx = a.first - b.first;
+    float dy = a.second - b.second;
+
+    return sqrt(dx * dx + dy * dy);
+}
+
+std::tuple<lemon::FullGraph*, lemon::FullGraph::EdgeMap<float>*> create_complete_graph(int n, std::vector<std::pair<float, float>>& vertices) {
+    lemon::FullGraph* g = new lemon::FullGraph(n);
+
+    lemon::FullGraph::EdgeMap<float>* cost = new lemon::FullGraph::EdgeMap<float>(*g);
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            lemon::FullGraphBase::Node u = (*g)(i);
+            lemon::FullGraphBase::Node v = (*g)(j);
+            float dist = vertices_distance(vertices[i], vertices[j]);
+            (*cost)[g->edge(u, v)] = dist;
+        }
+    }
+
+    return {g, cost};
 }
 
 int main(int argc, char* argv[]) {
     // test();
-  lemon::ListGraph g;
-  typedef lemon::ListGraph::Node Node;
-  typedef lemon::ListGraph::NodeIt NodeIt;
-  typedef lemon::ListGraph::Edge Edge;
-  typedef lemon::dim2::Point<int> Point;
-  
-  Node n1=g.addNode();
-  Node n2=g.addNode();
-  Node n3=g.addNode();
-  Node n4=g.addNode();
-  Node n5=g.addNode();
-  std::cout <<"foio";
+    std::vector<std::pair<float, float>> vertices = {
+        std::pair<float, float>(0.0, 0.0),
+        std::pair<float, float>(1.0, 0.0),
+        std::pair<float, float>(1.0, 1.0),
+        std::pair<float, float>(0.0, 1.0),
+    };
+
+    int n = 4;
+
+    lemon::FullGraph* g = nullptr;
+    lemon::FullGraph::EdgeMap<float>* cost = nullptr;
+
+    std::tie(g, cost) = create_complete_graph(n, vertices);
+
+    // for (int i = 0; i < g->edges().size(); i++)
+    //     std::cout << "edge" << std::endl;
+
+
+    // for( EdgeIt i(*g); i != lemon::INVALID; ++i ) {
+    //     std::cout << " " << g->edgeFromId(g->id(i)).;
+    // }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            lemon::FullGraphBase::Node u = (*g)(i);
+            lemon::FullGraphBase::Node v = (*g)(j);
+            std::cout << "i = " << i << ", j = " << j << " --- "  << (*cost)[g->edge(u, v)];
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout <<"foio";
     return 0;
 }
--- src/relaxed_solver_surviv_net.h
+++ src/relaxed_solver_surviv_net.h
@@ -8,12 +8,14 @@
 #include <queue>
 #include <string.h>
 
+#include <lemon/full_graph.h>
 
-class Flow {
+class Flow
+{
 public:
     int _n;
-    bool* _visited;
-    int* _parent_path;
+    bool *_visited;
+    int *_parent_path;
 
     Flow(int n)
         : _n(n)
@@ -23,24 +25,28 @@ public:
     }
 
     // max flow is greater than 2?
-    bool fordFulkerson(int source, int sink, double** residual_graph, int* path) {
+    bool fordFulkerson(int source, int sink, double **residual_graph, int *path)
+    {
         int u, v;
         path = _parent_path;
         memset(_parent_path, -1, sizeof(*_parent_path) * _n);
 
         double max_flow = 0.0f; // There is no flow initially
 
-        while (bfs(source, sink, residual_graph)) {
+        while (bfs(source, sink, residual_graph))
+        {
 
             double path_flow = std::numeric_limits<double>::max();
-            for (v = source; v != sink; v = _parent_path[v]) {
+            for (v = source; v != sink; v = _parent_path[v])
+            {
                 u = _parent_path[v];
                 path_flow = std::min(path_flow, residual_graph[u][v]);
             }
 
             // update residual capacities of the edges and
             // reverse edges along the path
-            for (v = source; v != sink; v = _parent_path[v]) {
+            for (v = source; v != sink; v = _parent_path[v])
+            {
                 u = _parent_path[v];
                 residual_graph[u][v] -= path_flow;
                 residual_graph[v][u] += path_flow; // talvez tenha que ser mudado
@@ -48,15 +54,16 @@ public:
 
             // Add path flow to overall flow
             max_flow += path_flow;
-            if (max_flow >= 2) {
+            if (max_flow >= 2)
+            {
                 return true;
             }
         }
         return false;
     }
 
-    
-    bool bfs(int source, int sink, double** residual_graph) {
+    bool bfs(int source, int sink, double **residual_graph)
+    {
 
         memset(_visited, 0, _n);
         std::queue<int> q;
@@ -65,13 +72,17 @@ public:
         _visited[source] = true;
         _parent_path[source] = -1;
 
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             int u = q.front();
             q.pop();
 
-            for (int v = 0; v < _n; v++) {
-                if (!_visited[v] && residual_graph[u][v] > 0) {
-                    if (v == sink) {
+            for (int v = 0; v < _n; v++)
+            {
+                if (!_visited[v] && residual_graph[u][v] > 0)
+                {
+                    if (v == sink)
+                    {
                         _parent_path[v] = u;
                         return true;
                     }
@@ -83,21 +94,23 @@ public:
         }
         return false;
     }
-
 };
 
-class connectivity_constrain : public GRBCallback {
+class connectivity_constrain : public GRBCallback
+{
 public:
-    GRBVar** _edges_vars;
-    double** _current_solution;
+    GRBVar **_edges_vars;
+    double **_current_solution;
     int _n;
     std::vector<int> _source_to_sink;
-    Flow* _flow;
+    Flow *_flow;
+    lemon::FullGraph *_graph;
 
-    connectivity_constrain(GRBVar** edges_vars, int n, const std::vector<int>& source_to_sink)
-        : _edges_vars(edges_vars), _n(n), _source_to_sink(source_to_sink)
+    connectivity_constrain(GRBVar **edges_vars, int n, const std::vector<int> &source_to_sink,
+                           lemon::FullGraph *graph, )
+        : _edges_vars(edges_vars), _n(n), _source_to_sink(source_to_sink), _graph(graph)
     {
-        _current_solution = new double* [_n];
+        _current_solution = new double *[_n];
         for (int i = 0; i < _n; i++)
             _current_solution[i] = new double[_n];
 
@@ -105,13 +118,15 @@ public:
     }
 
 protected:
-
-    double** update_current_solution() {
+    double **update_current_solution()
+    {
         for (int i = 0; i < _n; i++)
             _current_solution[i] = getSolution(_edges_vars[i], _n);
-        
-        for (int i = 0; i < _n; i++) {
-            for (int j = 0; j <= i; j++) {
+
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
                 _current_solution[i][j] = (_current_solution[i][j] >= 0.5) ? 1.0 : _current_solution[i][j];
             }
         }
@@ -119,24 +134,24 @@ protected:
         return _current_solution;
     }
 
-    void add_restrictions(const int* path) {
-
+    void add_restrictions(const int *path)
+    {
     }
 
+    void callback()
+    {
+        double **current_solution = update_current_solution();
+        int *path;
 
-    void callback() {
-        double** current_solution = update_current_solution();
-        int* path;
-
-        for(int i =  0; i < _source_to_sink.size(); i++) {
-            if (!_flow->fordFulkerson(i, _source_to_sink[i], current_solution, path)) {
+        for (int i = 0; i < _source_to_sink.size(); i++)
+        {
+            if (!_flow->fordFulkerson(i, _source_to_sink[i], current_solution, path))
+            {
                 // flow is not greater or equal to 2
                 // needs adicional restrictions
                 add_restrictions(path);
             }
         }
-        
-        
     }
 
     // void callback() {
@@ -161,84 +176,91 @@ protected:
     //         std::cout << "Error during callback" << std::endl;
     //     }
     // }
-
-
 };
 
-struct Vertex {
+struct Vertex
+{
     float x, y;
     // int degree = 0;
 
     Vertex(float x_, float y_) : x(x_), y(y_) {}
 };
 
-double distance(const Vertex& a, const Vertex& b) {
-    double dx = a.x - b.x;
-    double dy = a.y - b.y;
-
-    return sqrt(dx * dx + dy * dy);
-}
-
-class RelaxedSurvivableNetworkSolver {
+class RelaxedSurvivableNetworkSolver
+{
 
 public:
-    GRBVar** _edges_vars = nullptr;
-    GRBLinExpr* _degree_vars = nullptr;
+    GRBVar **_edges_vars = nullptr;
+    GRBLinExpr *_degree_vars = nullptr;
+    // lemon::FullGraph *_graph;
     int _n;
 
-	RelaxedSurvivableNetworkSolver(GRBModel& model, int n, const std::vector<int>& source_to_sink, const std::vector<Vertex>& vertices) {
+    RelaxedSurvivableNetworkSolver(
+        GRBModel &model, int n, const std::vector<int> &source_to_sink,
+        lemon::FullGraph *graph, lemon::FullGraph::EdgeMap<float> *cost)
+    {
         _n = n;
+        // _graph = graph;
 
-        _edges_vars = new GRBVar * [n];
+        _edges_vars = new GRBVar *[n];
         for (int i = 0; i < n; i++)
             _edges_vars[i] = new GRBVar[n];
 
-        _degree_vars = new GRBLinExpr [n];
+        _degree_vars = new GRBLinExpr[n];
 
         model.set(GRB_IntParam_LazyConstraints, 1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                _edges_vars[i][j] = model.addVar(0.0, 1.0, distance(vertices[i], vertices[j]),
-                    GRB_CONTINUOUS, "edge_" + std::to_string(i) + "_" + std::to_string(j));
-                _edges_vars[j][i] = _edges_vars[i][j];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                lemon::FullGraphBase::Node u = (*graph)(i);
+                lemon::FullGraphBase::Node v = (*graph)(j);
+                float dist = (*cost)[graph->edge(u, v)];
+                _edges_vars[i][j] = model.addVar(0.0, 2.0, dist, GRB_CONTINUOUS, "edge_" + std::to_string(i) + "_" + std::to_string(j));
+                // _edges_vars[j][i] = _edges_vars[i][j];
             }
         }
 
         // Forbid edge from node back to itself
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             _edges_vars[i][i].set(GRB_DoubleAttr_UB, 0);
         }
 
         // every vertex needs degree >= 2
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 _degree_vars[i] += _edges_vars[i][j];
             }
             model.addConstr(_degree_vars[i] >= 2);
         }
 
-        // connectivity_constrain cb = connectivity_constrain(_edges_vars, n, source_to_sink);
-        // model.setCallback(&cb);
+        connectivity_constrain cb = connectivity_constrain(_edges_vars, n, source_to_sink,
+                                                           graph);
+        model.setCallback(&cb);
 
         // model.optimize();
         // return;
-
-	}
-
-    ~RelaxedSurvivableNetworkSolver() {
-        
     }
 
-    void solve(GRBModel& model) {
+    ~RelaxedSurvivableNetworkSolver()
+    {
+    }
+
+    void solve(GRBModel &model)
+    {
         std::cout << "solve!" << std::endl;
         model.optimize();
 
-        double** sol = new double* [_n];
+        double **sol = new double *[_n];
         // for (int  i = 0; i  < _n; i++)
         //     sol[i] = new double[_n];
 
-        for (int i = 0; i < _n; i++) {
+        for (int i = 0; i < _n; i++)
+        {
             // for (int j = 0; j < n; j++) {
             sol[i] = model.get(GRB_DoubleAttr_X, _edges_vars[i], _n);
             // }
@@ -246,11 +268,11 @@ public:
         }
 
         for (int i = 0; i < _n; i++)
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j <= i; j++)
+            {
                 std::cout << "[" << std::to_string(i) << ", " << std::to_string(j);
                 std::cout << "] = " << sol[i][j] << std::endl;
             }
         // _edges_vars
     }
-
 };
